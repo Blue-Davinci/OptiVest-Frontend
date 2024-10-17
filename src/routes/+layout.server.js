6dcd376d0ffbce5f5f679787f39e3fd926e7c5ff
DaVinCi <52623427+Blue-Davinci@users.noreply.github.com>
2024-10-17 18:09:56 +0300

    Updated groupings for our routes.

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,13 +1,3 @@
-/*
-		event.locals.userrole = credentials.user.user_role;
-		event.countrycode = credentials.userinfo.country_code;
-		event.locals.currencycode = credentials.userinfo.currency_code;
-		event.locals.firstname = credentials.userinfo.first_name;
-        event.locals.lastname = credentials.userinfo.last_name;
-		event.locals.userimg = credentials.userinfo.profile_url;
-		event.locals.profilecompleted = credentials.userinfo.profile_completed;
-		event.locals.isAdmin = credentials.userinfo.user_role === 'admin';
-        */
 
 export const load = async ({ locals }) => {
     let userInformation = locals.userInfromation;
